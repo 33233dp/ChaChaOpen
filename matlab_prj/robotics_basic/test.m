f3f5bd82788bf8a6e8cc575e2dc2e38a54ed8e4c
33233dp <2918334521@qq.com>
2026-04-28 12:44:33 +0800

    20:44 2026年4月28日

--- matlab_prj/robotics_basic/test.m
+++ matlab_prj/robotics_basic/test.m
@@ -1,15 +1,54 @@
-% 定义矩阵 A
-A = [0 0 1 10; 
-     1 0 0 5; 
-     0 1 0 0;
-     0 0 0 1];
+% TWC = TW0 * TW06 * T6C
+% TWC = TWD * TDC
+TWD = [
+          1,0,0,830;
+          0,1,0,20; 
+          0,0,1,330;
+          0,0,0,1
+      ];
+TDC = [
+          cosd(35), -sind(35),0,-280;
+          sind(35), cosd(35), 0,250;
+          0,        0,        1,62.5;
+          0,        0,        0,1
+      ];
+TWC = TWD * TDC;
 
-d = [1,0,0.5];
-der = [0,0.1,0;];
+TW0 = [
+          1,0,0,0;
+          0,1,0,0;
+          0,0,1,373;
+          0,0,0,1
+      ];
+T6C = [
+          0,0,1,0;
+          0,-1,0,0;
+          1,0,0,206;
+          0,0,0,1
+      ];
 
-D = [d,der];
+T06 = TW0 \ TWC / T6C;
+P6o_ORG = T06(1:3,4);
+disp('P6o_ORG ='); disp(P6o_ORG);
 
-deta = differential_motion(D);
+[theta1, theta2, theta3] = pieper(-30,340,-40,-90,0,-90,0,0,338,P6o_ORG(1), P6o_ORG(2), P6o_ORG(3));
+disp('theta1 ='); disp(theta1);
+disp('theta2 ='); disp(theta2);
+disp('theta3 ='); disp(theta3);
 
-dA = deta * A;
-disp(dA);
+theta1 = 21.7166;
+theta2 =  -52.1867;
+theta3 =  2.4824;
+
+T01 = DH(0,0,0,theta1);
+T12 = DH(-30,-90,0,theta2);
+T23 = DH(340,0,0,theta3);
+T03 = T01 * T12 * T23;
+
+R03 = T03(1:3, 1:3);
+R06 = T06(1:3,1:3);
+R36 = R03' * R06;
+[phi, beta, psi] = inv_zyz(R36);
+disp('phi ='); disp(phi);
+disp('beta ='); disp(beta);   
+disp('psi ='); disp(psi);
